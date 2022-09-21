4ee42c7feaba5c86a16966d9b4160cc2259c9391
MushfiqMoon <mushfiq.moon013@google.com>
2022-09-21 10:27:46 +0600

    trying to implement the modal but cant fix it for now

--- src/Components/Countty/Country.js
+++ src/Components/Countty/Country.js
@@ -16,9 +16,29 @@ function Country(props) {
                     <p className="card-text">Population: {population}
                         <br />Region: {region}
                         <br />Area: {area}</p>
-                    <a href="#" className="btn btn-primary">View More</a>
+                    <a href="#" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">View More</a>
                 </div>
             </div>
+
+            {/* <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                            <h5 className="card-title">{name.common}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <p>Modal body text goes here.</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
         </>
 
 
