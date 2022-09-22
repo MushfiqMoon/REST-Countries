9d912282eda8a1600b2ed4ffc60b5860df52ad51
MushfiqMoon <mushfiq.moon013@google.com>
2022-09-23 05:46:53 +0600

    finally able to fetch individual data based on country

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -3,26 +3,45 @@ import Country from '../Countty/Country'
 
 export default function Countries() {
     const [countries, setCountries] = useState([])
+    const [cname, setCname] = useState('all')
     useEffect(() => {
-        fetch(`https://restcountries.com/v3.1/all`)
+        fetch(`https://restcountries.com/v3.1/${cname}`)
+        // fetch(`https://restcountries.com/v2/name/bangladesh`)
             .then(res => res.json())
             // .then(data => console.log(data))
             .then(data => setCountries(data))
-    }, [])
+    }, [cname])
+
+    // console.log(countries)
+
+    const selectCountry = (country) => {
+
+        const lower = country.toLowerCase()
+        const urlReady = "name/" + lower
+
+        setCname(urlReady)
+        // console.log(lower)
+        // console.log(urlReady)
+ 
+    }
+
+    console.log('mush', cname)
 
     return (
 
         <div className="container text-center mt-5">
             <div className="row align-items-start">
                 {
-                    countries.map((country, key) => 
 
-                        <div className="col-3">
-                            <Country data={country} key={country.ccn3}></Country>
+                    countries.map((country, key) =>
+
+                        <div className="col-4" key={country.name.common}>
+                            <Country data={country} selectCountry={selectCountry}></Country>
                         </div>
+
                     )
                 }
-           
+
             </div>
         </div>
     )
--- src/Components/Countty/Country.js
+++ src/Components/Countty/Country.js
@@ -1,26 +1,37 @@
 import React from 'react'
 
 function Country(props) {
-    console.log(props);
 
+    // console.log(props)
+    
     const { flags, name, population, region, area } = props.data
+    const { selectCountry } = props
+
+    // console.log(selectCountry);
 
     return (
         <>
+
             <div className="card mb-3">
-                <div className="p-2 border border-warning bg-warning">
-                    <img src={flags.svg} className="card-img-top" alt="..." />
-                </div>
-                <div className="card-body border border-warning">
-                    <h5 className="card-title">{name.common}</h5>
-                    <p className="card-text">Population: {population}
-                        <br />Region: {region}
-                        <br />Area: {area}</p>
-                    <a href="#" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">View More</a>
+                <div className="row g-0">
+                    <div className="col-md-4 border">
+                        <img src={flags.svg} className="card-img-top" alt="..." />
+                        <h5>{name?.common}</h5>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            
+                            <p className="card-text">Population: {population}
+                                <br />Region: {region}
+                                <br />Area: {area}</p>
+                            {/* <a href="#" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">View More</a> */}
+                            <a href="#" onClick={()=> selectCountry(name.common)} className="btn btn-warning">View More</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            {/* <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            {/* MODAL START */}
+            {/* <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-dialog">
                         <div className="modal-content">
